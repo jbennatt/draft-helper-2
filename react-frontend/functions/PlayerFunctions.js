d055d1b0092cb43c96b00431c07ddd865cb9538f
unknown <bennattj@gmail.com>
2023-08-22 23:50:12 -0500

    Removed logging--added some comments.

--- react-frontend/functions/PlayerFunctions.js
+++ react-frontend/functions/PlayerFunctions.js
@@ -8,8 +8,8 @@ import { allPositions, flex, wr, rb, te } from "./PlayerLabelFunctions"
  * @param {*} pickNum 
  * @param {*} draftPos 
  */
-export function enrichPlayers(allPlayers, draftedMap, pickNum, draftPos, numTeams, resetRank = false) {
-    // const _isYourPick = isYourPick(draftPos, numTeams)
+export function enrichPlayers(allPlayers, draftedMap, pickNum, draftPos,
+    numTeams, resetRank = false) {
     const _getPickBoundaries = getPickBoundaries(draftPos, numTeams)
     const _afterFirstPick = afterFirstPick(draftPos, numTeams)
     let playerCount = 1
@@ -28,7 +28,6 @@ export function enrichPlayers(allPlayers, draftedMap, pickNum, draftPos, numTeam
                 player.currentRank = playerCount
                 player.value = computeValue(player, pickNum)
             }
-            // player.isYourPick = _isYourPick(pickNum - 1 + player.currentRank)
             const newBoundaries = _getPickBoundaries(pickNum - 1 + player.currentRank)
 
             if (newBoundaries.match) {
@@ -40,10 +39,18 @@ export function enrichPlayers(allPlayers, draftedMap, pickNum, draftPos, numTeam
                 pickBoundaries = _getPickBoundaries(pickNum + player.currentRank)
             } else if (!areBoundariesEqual(newBoundaries.even, pickBoundaries.even) ||
                 !areBoundariesEqual(newBoundaries.odd, pickBoundaries.odd)) {
+                // if the boundaries have changed, you've moved to the next pick 
+                // and should mark this as "your pick"
                 player.isYourPick = true
-                pickBoundaries = newBoundaries
-            } else if (playerCount === 1 && _afterFirstPick(pickNum + player.currentRank - 1, pickNum)) {
+                pickBoundaries = newBoundaries // update current boundaries
+            } else if (playerCount === 1 &&
+                _afterFirstPick(pickNum + player.currentRank - 1, pickNum)) {
+                // the first player may not be a pick, but COULD be your next
+                // possible pick if their current rank is above your next pick.
                 player.isYourPick = true
+
+                // same as above for when we find a match--make sure next pick
+                //  doesn't register a change (only mark if it's an exact match)
                 pickBoundaries = _getPickBoundaries(pickNum + player.currentRank)
             }
 
@@ -98,7 +105,7 @@ export function togglePlayerDrafted(player, draftedMap, setDraftedMap, pickNum, 
     const drafted = draftedMap.get(player.name)
     if (pickNum && setPickNum) {
         if (drafted && pickNum > 1) setPickNum(pickNum - 1)
-        else if(!drafted) setPickNum(pickNum + 1)
+        else if (!drafted) setPickNum(pickNum + 1)
     }
     draftedMap.set(player.name, !drafted)
     setDraftedMap(new Map(draftedMap))
@@ -109,6 +116,17 @@ function stripNumFromPos(pos) {
     return strippedPos
 }
 
+/**
+ * Currently unused.
+ * 
+ * Returns a function that determines whether or not a pick number is "your pick"
+ * given your draft position and number of teams (the constructed function holds
+ * that information)
+ * 
+ * @param {U} draftPos 
+ * @param {*} numTeams 
+ * @returns 
+ */
 function isYourPick(draftPos, numTeams) {
     const secondRoundPos = numTeams + numTeams - draftPos + 1
     return pick => {
@@ -122,49 +140,81 @@ function isYourPick(draftPos, numTeams) {
     }
 }
 
-function getPickBoundaries(draftPos, numTeams, logging = false) {
+/**
+ * Returns a function that takes a pick number and finds the lower and upper
+ * bounds for both "odd" or "even" rounds AND whether or not that pick was 
+ * EXACTLY one of your picks.
+ * 
+ * @param {*} draftPos 
+ * @param {*} numTeams 
+ * @returns 
+ */
+function getPickBoundaries(draftPos, numTeams) {
     const secondRoundPos = numTeams + numTeams - draftPos + 1
     const teams2X = 2 * numTeams
 
     return pick => {
-        const closestEven = (pick - draftPos) / teams2X
-        const closestOdd = (pick - secondRoundPos) / teams2X
-        // console.log(`pick ${pick} for draft ${draftPos} and ${numTeams} teams: ${closestEven}`)
-        if (logging)
-            console.log(`${pick}: even: ${closestEven} -> [${Math.floor(closestEven)},${Math.ceil(closestEven)}], ${closestOdd} -> odd: [${Math.floor(closestOdd)},${Math.ceil(closestOdd)}]`)
+        /*
+         * We know that given d, a number which is DEFINITELY your pick, you
+         * will always have picks with number: d + 2n * N, where N is the number
+         * of teams.
+         * 
+         * We split your picks into "odds" and "evens" (literally since the 
+         * root draftPos corresponds to rounds 1, 3, etc. and the secondRoundPos
+         * corresponds to rounds 2, 4, etc.)
+         * 
+         * Solve equation pick = draftPos + 2n * N --> n = (pick - draftPos) / 2N
+         * 
+         * Same goes for even rounds (with root secondRounPos)
+         */
+        const closestOdd = (pick - draftPos) / teams2X
+        const closestEven = (pick - secondRoundPos) / teams2X
 
-        const match = pick === (draftPos + Math.round(closestEven) * teams2X) ||
-            pick === (secondRoundPos + Math.round(closestOdd) * teams2X)
+        // check whether or not there is an exact match: check both odd and even
+        const match = pick === (draftPos + Math.round(closestOdd) * teams2X) ||
+            pick === (secondRoundPos + Math.round(closestEven) * teams2X)
 
+        // return even and odd boundaries and whether or not it was an exact match
         return newPickBoundarySet(
-            newPickBoundaries(Math.floor(closestEven), Math.ceil(closestEven)),
             newPickBoundaries(Math.floor(closestOdd), Math.ceil(closestOdd)),
+            newPickBoundaries(Math.floor(closestEven), Math.ceil(closestEven)),
             match
         )
     }
 }
 
+/**
+ * Returns a function that takes the rank and pickNum to determine whether or
+ * not that rank occurs after your first pick.
+ * 
+ * @param {*} draftPos 
+ * @param {*} numTeams 
+ * @returns 
+ */
 function afterFirstPick(draftPos, numTeams) {
     const _getPickBoundaries = getPickBoundaries(draftPos, numTeams, true)
     const secondRoundPos = numTeams + numTeams - draftPos + 1
     const teams2X = 2 * numTeams
 
     return (rank, pickNum) => {
+        // find the closest pick that is your pick that's below this rank:
+
+        // get the odd and even boundaries, then pick the larger of the two lower bounds
         const bothBounds = _getPickBoundaries(rank)
         const lowerEven = draftPos + bothBounds.even.low * teams2X
         const lowerOdd = secondRoundPos + bothBounds.odd.low * teams2X
         const lowerClosest = lowerEven > lowerOdd ? lowerEven : lowerOdd
 
-        console.log(`lowerEven: ${lowerEven}, lowerOdd: ${lowerOdd}, pickNum: ${pickNum}`)
-
+        // if your pick below this rank is BEFORE the pick, you can pick this
+        // person up
         return lowerClosest > pickNum
     }
 }
 
-function newPickBoundarySet(evenBounds, oddBounds, match) {
+function newPickBoundarySet(oddBounds, evenBounds, match) {
     return {
-        'even': evenBounds,
         'odd': oddBounds,
+        'even': evenBounds,
         'match': match
     }
 }
