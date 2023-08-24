2b1c5e0f0232dfc0f9e534568faba3ea1f8d5e25
unknown <bennattj@gmail.com>
2023-08-24 00:29:56 -0500

    Cleaned up parentheses.

--- react-frontend/components/PlayerTable/PlayerLabel.js
+++ react-frontend/components/PlayerTable/PlayerLabel.js
@@ -4,17 +4,15 @@ import { togglePlayerDrafted } from '../../functions/PlayerFunctions'
 import labelStyles from '../../styles/PlayerTable/PlayerLabel.module.css'
 
 export default function PlayerLabel({ player, draftedMap, setDraftedMap, pickNum, setPickNum }) {
-    return (
-        <tr
-            className={`${labelStyles.player_label} ${getPosClass(stripNumFromPos(player.position))} ${player.isYourPick ? labelStyles.your_pick : ''} ${player.drafted ? labelStyles.drafted : labelStyles.undrafted}`}
-            onClick={() => togglePlayerDrafted(player, draftedMap, setDraftedMap, 
-                pickNum, setPickNum)}
-        >
-            {
-                headerFields.map(field =>
-                    <td key={field.header}>{player[field.playerKey]}</td>
-                )
-            }
-        </tr>
-    )
+    return <tr
+        className={`${labelStyles.player_label} ${getPosClass(stripNumFromPos(player.position))} ${player.isYourPick ? labelStyles.your_pick : ''} ${player.drafted ? labelStyles.drafted : labelStyles.undrafted}`}
+        onClick={() => togglePlayerDrafted(player, draftedMap, setDraftedMap,
+            pickNum, setPickNum)}
+    >
+        {
+            headerFields.map(field =>
+                <td key={field.header}>{player[field.playerKey]}</td>
+            )
+        }
+    </tr>
 }
--- react-frontend/components/SearchBar.js
+++ react-frontend/components/SearchBar.js
@@ -1,19 +1,17 @@
 import searchBarStyles from '../styles/SearchBar.module.css'
 
 export function SearchBar({ setSearchValue, includeDrafted, setIncludeDrafted }) {
-    return (
-        <div id='search_bar'>
-            <input placeholder='Enter Partial Name' id='google_complains_too_much'
-                onChange={event => setSearchValue(event.target.value)} />
-            <label className={searchBarStyles.drafted_label}>
-                include drafted
-                <input className={searchBarStyles.drafted_label}
-                    checked={includeDrafted}
-                    type='checkbox'
-                    onChange={() => { setIncludeDrafted(!includeDrafted) }} 
-                    id='this_also_needs_an_id'
-                    />
-            </label>
-        </div>
-    )
+    return <div id='search_bar'>
+        <input placeholder='Enter Partial Name' id='google_complains_too_much'
+            onChange={event => setSearchValue(event.target.value)} />
+        <label className={searchBarStyles.drafted_label}>
+            include drafted
+            <input className={searchBarStyles.drafted_label}
+                checked={includeDrafted}
+                type='checkbox'
+                onChange={() => { setIncludeDrafted(!includeDrafted) }}
+                id='this_also_needs_an_id'
+            />
+        </label>
+    </div>
 }
